e58823b39154442f67af88f3deb6e09073510d91
Marcos Corsi <marcosvcorsi@gmail.com>
2022-04-09 21:17:24 -0300

    feat: adjust to apollo federation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,37 @@
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+        context: ({ req }) => ({ headers: req.headers }),
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'purchases', url: 'http://localhost:3333/graphql' },
+            { name: 'classroom', url: 'http://localhost:3334/graphql' },
+          ],
+        }),
+        buildService: ({ name, url }) => {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest: ({ request, context }) => {
+              request.http.headers.set(
+                'authorization',
+                context?.['headers']?.['authorization'],
+              );
+            },
+          });
+        },
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
